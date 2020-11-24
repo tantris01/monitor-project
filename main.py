912945d0c7081ba8f7777980306a921743d12683
Tristan <74166543+tantris01@users.noreply.github.com>
2020-11-24 23:30:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def send_notification(posts):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'top-secret!'
     app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
-    app.config['MAIL_PORT'] = 25
-    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USERNAME'] = 'apikey'
     app.config['MAIL_PASSWORD'] = environ.get('SENDGRID_API_KEY')
     app.config['MAIL_DEFAULT_SENDER'] = environ.get('MAIL_DEFAULT_SENDER')
