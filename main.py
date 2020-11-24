8d4acf3f1ff6707ba9200a12d2cbefd79679f00f
Tristan <74166543+tantris01@users.noreply.github.com>
2020-11-24 23:33:56 +1100

    Update main.py

--- main.py
+++ main.py
@@ -45,17 +45,17 @@ def send_notification(posts):
     app.config['SECRET_KEY'] = 'top-secret!'
     app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
     app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'apikey'
-    app.config['MAIL_PASSWORD'] = environ.get('SENDGRID_API_KEY')
-    app.config['MAIL_DEFAULT_SENDER'] = environ.get('MAIL_DEFAULT_SENDER')
+    app.config['MAIL_PASSWORD'] = environ['SENDGRID_API_KEY']
+    app.config['MAIL_DEFAULT_SENDER'] = environ['MAIL_DEFAULT_SENDER]
     mail = Mail(app)
 
     today_date = datetime.date.today().strftime("%d:%m:%Y")
     subject = f'monitors {today_date}'
 
     with app.app_context():
-        msg = Message(subject, recipients=[environ.get('MAIL_DEFAULT_SENDER')])
+        msg = Message(subject, recipients=[environ['MAIL_DEFAULT_SENDER']])
         msg.body = posts
         mail.send(msg)
 
